ffce606235577b72bd5896551aad31ea308f2af5
jonathan4747 <harold307lat@gmail.com>
2022-01-17 13:46:47 -0500

    imprimir variables y cadenas

---
+++ imprimir.py
@@ -0,0 +1,15 @@
+print("Hola, mundo")
+
+name = "Jonathan"
+print( "Hola," , name)
+print( "Hola, " + name)
+
+num=36
+print( "Hola",num,"!")
+print( "Hola " + str(num) + "!")
+
+comida1 = "ceviche"
+comida2 = "arroz chaufa"
+
+print("Amo comer {} y {}".format(comida1,comida2))
+print(f'Amo comer {comida1} y {comida2}')
